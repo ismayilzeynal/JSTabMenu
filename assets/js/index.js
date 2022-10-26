25f9d166f3e52d8f9c4c29f1f9dc84dd8dffc1e3
ismayilzeynal <ismayilbz@code.edu.az>
2022-10-26 18:07:19 +0400

    basic form completed

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const accardions = document.querySelectorAll(".accordion");
+
+accardions.forEach(acc => {
+    acc.addEventListener("click", function () {
+        let open = this.nextElementSibling;
+        if (open.style.display === "block") {
+            open.style.display = "none";
+        } else {
+            open.style.display = "block";
+        }
+    })
+});
